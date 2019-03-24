2f66f9865843d94c5c6183e7a9fd2db0bcdce38d
Lorenzo Cazzoli <lorenzo.cazzoli@studio.unibo.it>
2019-03-24 01:46:50 +0100

    Create Welcome components

---
+++ components/Welcome.jsx
@@ -0,0 +1,94 @@
+// import Link from 'next/Link';
+import {transparentize} from 'polished';
+import styled, {media} from '../utils/ThemedComponent';
+
+const TitleGrid = styled.div`
+    display: grid;
+    grid-template-rows: 20rem auto;
+    grid-gap: 2.5rem;
+    justify-items: center;
+
+    box-sizing: border-box;
+    padding: 1rem;
+    min-height: 100vh;
+
+    ${media.tablet`
+        grid-template-rows: auto;
+        grid-template-columns: 1fr 1fr;
+        align-items: center;
+    `}
+
+    ${media.hd`
+        height: 100vh;
+        width: 70vw;
+    `}
+`;
+
+const Image = styled.img`
+    height: 20rem;
+
+    ${media.tablet`
+        height: 34rem;
+    `}
+
+    ${media.hd`
+        height: 45rem;
+    `}
+`;
+
+const Title = styled.span`
+    font-size: 1.25rem;
+    line-height: 1.875rem;
+    font-weight: bold;
+    color: ${props => props.theme.color.black_bean};
+
+    width: 100%;
+
+    ${media.hd`
+        font-size: 1.5rem;
+        line-height: 2.25rem;
+    `}
+
+`;
+
+const TextContainer = styled.div`
+    color: ${props => props.theme.color.dark_liver};
+    background-color: ${props => transparentize(.5, props.theme.color.white)};
+
+    padding: 2rem;
+    border-radius: 10px;
+
+    font-size: 1rem;
+    line-height: 1.5rem;
+    font-weight: 400;
+
+    box-sizing: border-box;
+    width: 60vw;
+`;
+
+const Welcome = () =>
+{
+    return (<TitleGrid>
+        <Image src='static/ritratto_copertina.png' />
+        <Title>24 anni,
+            <br />una bicicletta sotto ai piedi
+            <br />e un rosario tra le mani.
+            <br />Una laurea in agraria,
+            <br />una fidanzata,
+            <br />un ideale di vita cristiano.
+            <br />Una famiglia numerosa,
+            <br />un progetto per i lavoratori agricoli,
+            <br />la speranza di una giustizia sociale.
+            <br />La morte per omicidio
+            <br />sopraggiunta all’1.50 a fermare tutto.
+            <br />O a trasformare tutto...
+            <br />
+            <br />Servo di Dio,
+            <br />luce e ispirazione per i giovani
+            <br />ad avere il coraggio di mettere tutto se stessi
+            <br />per cambiare il mondo.
+        </Title>
+    </TitleGrid>);
+}
+
+export default Welcome;
--- pages/index.js
+++ pages/index.js
@@ -1,56 +1,8 @@
 // import Link from 'next/Link';
 import {transparentize} from 'polished';
-import styled, {media} from '../utils/ThemedComponent';
+import styled from '../utils/ThemedComponent';
 import Layout from '../components/Layout';
-
-const TitleGrid = styled.div`
-    display: grid;
-    grid-template-rows: 20rem auto;
-    grid-gap: 2.5rem;
-    justify-items: center;
-
-    box-sizing: border-box;
-    padding: 1rem;
-    min-height: 100vh;
-
-    ${media.tablet`
-        grid-template-rows: auto;
-        grid-template-columns: 1fr 1fr;
-        align-items: center;
-    `}
-
-    ${media.hd`
-        height: 100vh;
-        width: 70vw;
-    `}
-`;
-
-const Image = styled.img`
-    height: 20rem;
-
-    ${media.tablet`
-        height: 34rem;
-    `}
-
-    ${media.hd`
-        height: 45rem;
-    `}
-`;
-
-const Title = styled.span`
-    font-size: 1.25rem;
-    line-height: 1.875rem;
-    font-weight: bold;
-    color: ${props => props.theme.color.black_bean};
-
-    width: 100%;
-
-    ${media.hd`
-        font-size: 1.5rem;
-        line-height: 2.25rem;
-    `}
-
-`;
+import Welcome from '../components/Welcome';
 
 const TextContainer = styled.div`
     color: ${props => props.theme.color.dark_liver};
@@ -68,27 +20,7 @@ const TextContainer = styled.div`
 `;
 
 const Index = () => (<Layout>
-    <TitleGrid>
-        <Image src='static/ritratto_copertina.png' />
-        <Title>24 anni,
-            <br />una bicicletta sotto ai piedi
-            <br />e un rosario tra le mani.
-            <br />Una laurea in agraria,
-            <br />una fidanzata,
-            <br />un ideale di vita cristiano.
-            <br />Una famiglia numerosa,
-            <br />un progetto per i lavoratori agricoli,
-            <br />la speranza di una giustizia sociale.
-            <br />La morte per omicidio
-            <br />sopraggiunta all’1.50 a fermare tutto.
-            <br />O a trasformare tutto...
-            <br />
-            <br />Servo di Dio,
-            <br />luce e ispirazione per i giovani
-            <br />ad avere il coraggio di mettere tutto se stessi
-            <br />per cambiare il mondo.
-        </Title>
-    </TitleGrid>
+    <Welcome />
     <TextContainer>
         <h2>La strada di Giuseppe Fanin </h2>
         <h3>24 anni per la santità</h3>
